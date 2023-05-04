fd8be2fc316dffeb35d58bcbf7de253785a2becd
Nocab0819 <penny013101310131@gmail.com>
2023-05-04 14:29:00 +0800

    @app.route("/")

--- api/index.py
+++ api/index.py
@@ -15,6 +15,9 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('F3iWzws3ypon002uN+swy+EBDOsVnjl79FXkhKkd4Ev7ZfBGixANYB9kSuSNLX/GhbiPirq/w8AD2Bo8O4Ctp/Bq3EXGluy0DseZR4+zCF9jsgoqsjEYVhsCSfpLZ3HEA+5UcrajxljyahU/audoKgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('c1e3f01970d9f2828273807a3b9cab6e')
 
+@app.route("/")
+def home():
+    return "小羅伯特活著"
 
 @app.route("/callback", methods=['POST'])
 def callback():
